70380941dfd5ba23f7dbc18098d28422364177cc
Xavid Ramirez <xavidram@hotmail.com>
2016-12-04 21:57:29 -0600

    pusing initial and final

---
+++ app/src/main/java/com/xavidram/stickercv_demo/MainActivity.java
@@ -0,0 +1,150 @@
+package com.xavidram.stickercv_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import org.opencv.android.*;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener {
+
+
+    private CameraBridgeViewBase openCvCameraView;
+    private CascadeClassifier cascadeClassifier;
+    private Mat grayscaleImage;
+    private int absolutestickerSize;
+    private static final String TAG = "MainActivity";
+
+    static {
+        if(!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV Not Found");
+        }else {
+            Log.d(TAG, "OpenCV Found");
+        }
+    }
+
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    initializeOpenCVDependencies();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+
+    private void initializeOpenCVDependencies() {
+
+
+        try {
+            // Copy the resource into a temp file so OpenCV can load it
+            InputStream is = getResources().openRawResource(R.raw.permit1_stage11);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mCascadeFile = new File(cascadeDir, "permit1_stage11.xml");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+
+            // Load the cascade classifier
+            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            cascadeClassifier.load(mCascadeFile.getAbsolutePath());
+        } catch (Exception e) {
+            Log.e("OpenCVActivity", "Error loading cascade", e);
+        }
+
+
+        // And we are ready to go
+        openCvCameraView.enableView();
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        openCvCameraView = new JavaCameraView(this, -1);
+        setContentView(openCvCameraView);
+        openCvCameraView.setCvCameraViewListener(this);
+
+    }
+
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        grayscaleImage = new Mat(height, width, CvType.CV_8UC4);
+
+
+        // The stickers will be a 20% of the height of the screen
+        absolutestickerSize = (int) (height * 0.2);
+    }
+
+
+    @Override
+    public void onCameraViewStopped() {
+    }
+
+
+    @Override
+    public Mat onCameraFrame(Mat aInputFrame) {
+        // Create a grayscale image
+        Imgproc.cvtColor(aInputFrame, grayscaleImage, Imgproc.COLOR_RGBA2RGB);
+
+
+        MatOfRect stickers = new MatOfRect();
+
+
+        // Use the classifier to detect stickers
+        /* Old cascade find
+        if (cascadeClassifier != null) {
+            cascadeClassifier.detectMultiScale(grayscaleImage, stickers, 1.2, 5, 2,
+                    new Size(absolutestickerSize, absolutestickerSize), new Size());
+        }
+        */
+        if(cascadeClassifier != null) {
+            cascadeClassifier.detectMultiScale(grayscaleImage, stickers, 1.3,5,2,new Size(10,10),new Size(100,100));
+        }
+
+
+        // If there are any stickers found, draw a rectangle around it
+        Rect[] stickersArray = stickers.toArray();
+        for (int i = 0; i <stickersArray.length; i++)
+            Imgproc.rectangle(aInputFrame, stickersArray[i].tl(), stickersArray[i].br(), new Scalar(0, 255, 0, 255), 3);
+
+
+        return aInputFrame;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
+    }
+}
